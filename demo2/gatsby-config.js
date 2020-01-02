381575214b7512845c1dc2e56c6a786c39e15ec2
antoniomgatto <antonio.gatto@gobee.com.br>
2020-01-02 17:23:13 -0300

    adding info from markdown files to pages

--- demo2/gatsby-config.js
+++ demo2/gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Panda eating lots',
+    title: "Panda eating lots",
   },
   plugins: [
     {
@@ -10,6 +10,7 @@ module.exports = {
         path: `${__dirname}/src/`,
       },
     },
+    `gatsby-transformer-remark`,
     `gatsby-plugin-emotion`,
     {
       resolve: `gatsby-plugin-typography`,
--- demo2/src/pages/index.js
+++ demo2/src/pages/index.js
@@ -1,7 +1,12 @@
 import React from "react";
 import { Layout } from '../components';
+import { graphql } from 'gatsby';
+import { css } from '@emotion/core';
+import { rhythm } from '../utils/typography';
 
-export default () => (
+console.log(rhythm(0.25));
+
+export default ({ data }) => (
   <Layout>
     <h1>Amazing Pandas Eating Things</h1>
     <div>
@@ -10,5 +15,45 @@ export default () => (
         alt="Group of pandas eating bamboo"
       />
     </div>
+
+    <div>
+      <h4>Blog posts</h4>
+      {
+        data.allMarkdownRemark.edges.map(({ node }) => (
+          <div key={node.id}>
+            <h3
+              css={css`margin-bottom: ${rhythm(0.25)}`}
+            >
+              { node.frontmatter.title}{' '}
+              <span
+                css={css`
+                  color: #bbb
+                `}
+              >
+                - { node.frontmatter.date }
+              </span>
+              </h3>
+            <p>{ node.excerpt }</p>
+          </div>
+        ))
+      }
+    </div>
   </Layout>
 )
+
+export const query = graphql`
+  query {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC}) {
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "DD MMMM, YYYY")
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`;
